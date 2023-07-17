f6a6998c6458002228867e24c95779a6af43c6e6
MD MAQSOOD ALI <138513527+maq2510@users.noreply.github.com>
2023-07-17 21:31:13 +0530

    Update moindairyfarm.py

--- moindairyfarm.py
+++ moindairyfarm.py
@@ -161,17 +161,3 @@ def get_table_download_link(df):
     b64 = base64.b64encode(buffer.read()).decode()
     return f'<a href="data:application/octet-stream;base64,{b64}" download="customer_records.xlsx">Download Customer Records</a>'
 
-try:
-    existing_records = pd.read_excel("customer_records.xlsx")
-    existing_records.set_index("Entry No", inplace=True)  # Set "Entry No" as the index
-    if "Date" in existing_records.columns:
-        st.dataframe(existing_records.drop(columns=["Date"]))  # Drop the "Date" column from display
-    else:
-        st.dataframe(existing_records)
-
-    # Show the download link if records are available
-    st.markdown(get_table_download_link(existing_records), unsafe_allow_html=True)
-
-except FileNotFoundError:
-    st.warning("No customer records found. Start by entering new customer details.")
-
