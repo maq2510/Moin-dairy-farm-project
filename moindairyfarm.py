67dabea39ca2871452ee7b3d882780a7ddd91e2a
MD MAQSOOD ALI <138513527+maq2510@users.noreply.github.com>
2023-07-18 14:19:26 +0530

    Update moindairyfarm.py

--- moindairyfarm.py
+++ moindairyfarm.py
@@ -22,106 +22,115 @@ def update_current_date():
 
 # Input fields
 customer_name = st.text_input("Customer Name", "")
-frequency = st.radio("Frequency", ["Monthly", "Daily"])
 open_close_status = st.radio("Open/Close Status", ["Open", "Close"])
-liters_purchased = st.selectbox("Liters of Milk Purchased", [i * 0.5 for i in range(1, 51)])
-rate_per_liter_options = [50, 60, 70]
-rate_per_liter = st.selectbox("Rate per Liter (INR)", rate_per_liter_options)
 
-# Calculate the bill amount based on liters purchased and rate per liter
-bill_amount_rupees = liters_purchased * rate_per_liter
-if frequency == "Monthly":
-    bill_amount_rupees *= 30  # Assuming it's for a month
+# Check if the customer is open or closed
+if open_close_status == "Open":
+    frequency = st.radio("Frequency", ["Monthly", "Daily"])
+    liters_purchased = st.selectbox("Liters of Milk Purchased", [i * 0.5 for i in range(1, 51)])
+    rate_per_liter_options = [50, 60, 70]
+    rate_per_liter = st.selectbox("Rate per Liter (INR)", rate_per_liter_options)
 
-# Payment status (Paid or Due)
-if frequency == "Monthly":
-    payment_status = "Monthly"
-else:
-    payment_status = st.radio("Payment Status", ["Paid", "Due"])
+    # Calculate the bill amount based on liters purchased and rate per liter
+    bill_amount_rupees = liters_purchased * rate_per_liter
+    if frequency == "Monthly":
+        bill_amount_rupees *= 30  # Assuming it's for a month
 
-# Remark
-if frequency == "Monthly":
-    remark = ""
-else:
-    remark = st.text_input("Remark", "")
+    st.write(f"Bill Amount (INR): {bill_amount_rupees:.2f}")
 
-# Current date
-current_date = update_current_date()
+    # Payment status (Paid or Due)
+    if frequency == "Monthly":
+        payment_status = "Monthly"
+    else:
+        payment_status = st.radio("Payment Status", ["Paid", "Due"])
 
-# Submit button for "Open" customers
-if open_close_status == "Open" and st.button("Submit"):
-    # Save customer details to a DataFrame
-    data = {
-        "Entry No": [1],  # Start index from 1
-        "Customer Name": [customer_name],
-        "Frequency": [frequency],
-        "Open/Close": [open_close_status],
-        "Liters of Milk Purchased": [liters_purchased],
-        "Rate per Liter (INR)": [rate_per_liter],
-        "Bill Amount (INR)": [bill_amount_rupees],
-        "Payment Status": [payment_status],
-        "Remark": [remark],
-        "Date": [current_date],  # Add the current date
-    }
-    new_entry = pd.DataFrame(data)
+    # Remark
+    if frequency == "Monthly":
+        remark = ""
+    else:
+        remark = st.text_input("Remark", "")
 
-    # Check if the Excel file exists, if not, create it
-    try:
-        existing_records = pd.read_excel("customer_records.xlsx")
-        entry_no = existing_records["Entry No"].max() + 1  # Increment the entry number
-        new_entry["Entry No"] = entry_no
+    # Current date
+    current_date = update_current_date()
+    st.write(f"Current Date: {current_date}")
 
-        # Check if the month has changed
-        if not pd.isnull(existing_records["Date"]).any():
-            last_date = pd.to_datetime(existing_records["Date"]).max()
-            if last_date.month != pd.to_datetime(current_date).month:
-                # Create a new DataFrame for the next month
-                updated_records = new_entry
+    # Submit button for "Open" customers
+    if st.button("Submit"):
+        # Save customer details to a DataFrame
+        data = {
+            "Entry No": [1],  # Start index from 1
+            "Customer Name": [customer_name],
+            "Frequency": [frequency],
+            "Open/Close": [open_close_status],
+            "Liters of Milk Purchased": [liters_purchased],
+            "Rate per Liter (INR)": [rate_per_liter],
+            "Bill Amount (INR)": [bill_amount_rupees],
+            "Payment Status": [payment_status],
+            "Remark": [remark],
+            "Date": [current_date],  # Add the current date
+        }
+        new_entry = pd.DataFrame(data)
+
+        # Check if the Excel file exists, if not, create it
+        try:
+            existing_records = pd.read_excel("customer_records.xlsx")
+            entry_no = existing_records["Entry No"].max() + 1  # Increment the entry number
+            new_entry["Entry No"] = entry_no
+
+            # Check if the month has changed
+            if not pd.isnull(existing_records["Date"]).any():
+                last_date = pd.to_datetime(existing_records["Date"]).max()
+                if last_date.month != pd.to_datetime(current_date).month:
+                    # Create a new DataFrame for the next month
+                    updated_records = new_entry
+                else:
+                    updated_records = pd.concat([existing_records, new_entry], ignore_index=True)
             else:
-                updated_records = pd.concat([existing_records, new_entry], ignore_index=True)
-        else:
+                updated_records = new_entry
+
+        except FileNotFoundError:
             updated_records = new_entry
 
-    except FileNotFoundError:
-        updated_records = new_entry
+        # Save the DataFrame to an Excel file
+        updated_records.to_excel("customer_records.xlsx", index=False)
 
-    # Save the DataFrame to an Excel file
-    updated_records.to_excel("customer_records.xlsx", index=False)
+        # Show success message
+        st.success("Customer details saved successfully!")
 
-    # Show success message
-    st.success("Customer details saved successfully!")
+else:
+    # Submit button for "Close" customers
+    if st.button("Close Customer"):
+        frequency = st.radio("Frequency", ["Monthly", "Daily"])
+        # Save customer details to a DataFrame for "Closed" customer
+        data = {
+            "Entry No": [1],  # Start index from 1
+            "Customer Name": [customer_name],
+            "Frequency": [frequency],
+            "Open/Close": [open_close_status],
+            "Liters of Milk Purchased": ["na"],
+            "Rate per Liter (INR)": ["na"],
+            "Bill Amount (INR)": ["na"],
+            "Payment Status": ["na"],
+            "Remark": [""],
+            "Date": [datetime.today().strftime("%d/%B/%Y")],  # Add the current date
+        }
+        new_entry = pd.DataFrame(data)
 
-elif open_close_status == "Close" and st.button("Close Customer"):
-    # Save customer details to a DataFrame for "Closed" customer
-    data = {
-        "Entry No": [1],  # Start index from 1
-        "Customer Name": [customer_name],
-        "Frequency": [frequency],
-        "Open/Close": [open_close_status],
-        "Liters of Milk Purchased": ["na"],
-        "Rate per Liter (INR)": ["na"],
-        "Bill Amount (INR)": ["na"],
-        "Payment Status": ["na"],
-        "Remark": [""],
-        "Date": [datetime.today().strftime("%d/%B/%Y")],  # Add the current date
-    }
-    new_entry = pd.DataFrame(data)
+        # Check if the Excel file exists, if not, create it
+        try:
+            existing_records = pd.read_excel("customer_records.xlsx")
+            entry_no = existing_records["Entry No"].max() + 1  # Increment the entry number
+            new_entry["Entry No"] = entry_no
+            updated_records = pd.concat([existing_records, new_entry], ignore_index=True)
 
-    # Check if the Excel file exists, if not, create it
-    try:
-        existing_records = pd.read_excel("customer_records.xlsx")
-        entry_no = existing_records["Entry No"].max() + 1  # Increment the entry number
-        new_entry["Entry No"] = entry_no
-        updated_records = pd.concat([existing_records, new_entry], ignore_index=True)
+        except FileNotFoundError:
+            updated_records = new_entry
 
-    except FileNotFoundError:
-        updated_records = new_entry
+        # Save the DataFrame to an Excel file
+        updated_records.to_excel("customer_records.xlsx", index=False)
 
-    # Save the DataFrame to an Excel file
-    updated_records.to_excel("customer_records.xlsx", index=False)
-
-    # Show success message
-    st.success("Customer status updated to 'Close'!")
+        # Show success message
+        st.success("Customer status updated to 'Close'!")
 
 # Download link for Excel file
 def get_table_download_link(df):
